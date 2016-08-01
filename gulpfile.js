0f75fd3eecad5e1f170d900fd4e1b8c4ee11295e
angeloLuna <angel.luna.najera@gmail.com>
2016-07-31 22:29:16 -0500

    Gulpfile with copy-task, prune-task, sass-task and minify-css and an example files and folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,33 @@
 var gulp = require('gulp');
-// var watch = require('gulp-watch');
-// var sass = require('gulp-sass');
-// var mini = require('gulp-minify-css');
-// var prune = require('gulp-prune');
-// var newer = require('gulp-newer');
-
-
-// gulp.task('sass', function () {
-//   return gulp.src('./src/assets/styles/sass/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./src/assets/styles/css'));
-// });
-
-// gulp.task('mini', ['sass'], function () {
-//     gulp.src('./src/assets/styles/css/*.css')
-//     .pipe(mini())
-//     .pipe(gulp.dest('./dist/assets/styles/css'))
-// });
-
-
+var prune = require('gulp-prune');
+var newer = require('gulp-newer');
+var sass = require('gulp-sass');
+var mini = require('gulp-minify-css');
+// Tarea para copiar src a dist
 gulp.task('copy', function() {
-    gulp.src(['./src/**', '!./src/assets/styles/**', './src/*'])
-    .pipe(gulp.dest('./dist/'))
+    gulp.src(['src/**/*', '!src/assets/styles/**/'])
+    .pipe(gulp.dest('dist/'))
+}); 
+// Tarea para borrar archivos de dist cuando se borran en src
+gulp.task('prune', ['copy'], function() {
+    gulp.src(['src/**/*', '!src/assets/styles/**/'])
+    .pipe(prune('dist/')) 
+    .pipe(newer('dist/'))   
+    .pipe(gulp.dest('dist'))
 }); 
 
-// gulp.task('prune', ['copy'], function() {
-//     gulp.src('./src/**')
-//     .pipe(prune('./dist/')) 
-//     .pipe(newer('./dist/'))   
-//     .pipe(gulp.dest('./dist'))   
-// }); 
+gulp.task('sass', function () {
+  return gulp.src('src/assets/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/assets/'));
+});
 
- 
+gulp.task('mini', ['sass'], function () {
+    gulp.src('src/assets/styles/sass/*.css')
+    .pipe(mini())
+    .pipe(gulp.dest('dist/assets/styles/css'))
+});
+
 gulp.task('default', function () { 
-    gulp.watch('./src/**', ['copy']); 
+    gulp.watch('src/**/*', ['copy', 'prune', 'sass', 'mini']); 
 });  
