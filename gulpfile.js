4a75b56c5535ca75e17766cf2e85cce965b06e03
angeloLuna <angel.luna.najera@gmail.com>
2016-08-26 15:36:33 -0500

    added Livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ var prune = require('gulp-prune');
 var newer = require('gulp-newer');
 var sass = require('gulp-sass');
 var mini = require('gulp-minify-css');
+var livereload = require('gulp-livereload');
 // Tarea para copiar src a dist
 gulp.task('copy', function() {
     gulp.src(['src/**/*', '!src/assets/styles/**/'])
     .pipe(gulp.dest('dist/'))
+    .pipe(livereload());
 }); 
 // Tarea para borrar archivos de dist cuando se borran en src
 gulp.task('prune', ['copy'], function() {
@@ -14,20 +16,25 @@ gulp.task('prune', ['copy'], function() {
     .pipe(prune('dist/')) 
     .pipe(newer('dist/'))   
     .pipe(gulp.dest('dist'))
+    .pipe(livereload());
 }); 
 
 gulp.task('sass', function () {
   return gulp.src('src/assets/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/assets/'));
+    .pipe(gulp.dest('src/assets/'))
+    .pipe(livereload());
 });
 
 gulp.task('mini', ['sass'], function () {
     gulp.src('src/assets/styles/sass/*.css')
     .pipe(mini())
     .pipe(gulp.dest('dist/assets/styles/css'))
+    .pipe(livereload());
 });
 
-gulp.task('default', function () { 
+gulp.task('default', function () {
+    livereload({ start: true })
+    livereload.listen()
     gulp.watch('src/**/*', ['copy', 'prune', 'sass', 'mini']); 
 });  
